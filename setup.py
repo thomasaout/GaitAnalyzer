3e17110b2d98a10348f1ac44c6acb0585d2a9bcc
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-01-29 12:06:28 +0400

    added tests skeleton

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="GaitAnalyzer",
+    version=1.0,
+    author="laboratoireIRISSE",
+    author_email="eve.charbonneau.1@umontreal.ca",
+    description="A toolbox for biomechanical analysis of gait",
+    long_description=long_description,
+    url="https://github.com/laboratoireIRISSE/GaitAnalyzer",
+    packages=[
+        ".",
+        "gait_analyzer",
+        "examples",
+    ],
+    license="LICENSE",
+    keywords=["Walking", "Biomechanics", "Gait parameters", "Functional Electrical Stimulation", "Motion capture", "Ground reaction force"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    include_package_data=True,
+    python_requires=">=3.7",
+    zip_safe=False,
+)
---
+++ tests/test__events.py
@@ -0,0 +1,8 @@
+import pytest
+import numpy an np
+
+
+# Example of test to perform
+a = 1.0
+np.assert_almost_equal(a, 1.0)
+
