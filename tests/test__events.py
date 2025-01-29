5aa70bbc8f09d7549b0228c76d2d71e46c194bb9
Charbie <eve.charbonneau.1@umontreal.ca>
2025-01-29 13:48:57 +0400

    .

--- tests/test__events.py
+++ tests/test__events.py
@@ -1,6 +1,7 @@
 import numpy.testing as npt
 
 
-# Example of test to perform
-a = 1.0
-npt.assert_almost_equal(a, 1.0)
+def dummy_test():
+    # Example of test to perform
+    a = 1.0
+    npt.assert_almost_equal(a, 1.0)
