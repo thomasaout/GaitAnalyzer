3e5aed42bcf5e3f4f3b8d08bf1b788b38298aacf
Charbie <eve.charbonneau.1@umontreal.ca>
2025-02-05 11:07:29 +0400

    dealt with platform data :)

--- gait_analyzer/experimental_data.py
+++ gait_analyzer/experimental_data.py
@@ -3,6 +3,7 @@ import biorbd
 import numpy as np
 
 from gait_analyzer.biomod_model_creator import BiomodModelCreator
+from gait_analyzer.operator import Operator
 
 
 class ExperimentalData:
@@ -43,9 +44,11 @@ class ExperimentalData:
         self.result_folder = result_folder
 
         # Extended attributes
+        self.c3d = None
         self.model_marker_names = None
         self.marker_sampling_frequency = None
         self.markers_dt = None
+        self.marker_units = None
         self.nb_marker_frames = None
         self.markers_sorted = None
         self.markers_sorted_with_virtual = None
@@ -53,6 +56,7 @@ class ExperimentalData:
         self.analogs_dt = None
         self.nb_analog_frames = None
         self.f_ext_sorted = None
+        self.f_ext_sorted_filtered = None
         self.markers_time_vector = None
         self.analogs_time_vector = None
 
@@ -71,15 +75,15 @@ class ExperimentalData:
             # model_muscle_names = [m.to_string() for m in self.biorbd_model_creator.biorbd_model.muscleNames()]
 
         def sort_markers():
-            self.c3d = ezc3d.c3d(self.c3d_full_file_path)
+            self.c3d = ezc3d.c3d(self.c3d_full_file_path, extract_forceplat_data=True)
             markers = self.c3d["data"]["points"]
             self.marker_sampling_frequency = self.c3d["parameters"]["POINT"]["RATE"]["value"][0]  # Hz
             self.markers_dt = 1 / self.c3d["header"]["points"]["frame_rate"]
             self.nb_marker_frames = markers.shape[2]
             exp_marker_names = self.c3d["parameters"]["POINT"]["LABELS"]["value"]
-            marker_units = 1
+            self.marker_units = 1
             if self.c3d["parameters"]["POINT"]["UNITS"]["value"][0] == "mm":
-                marker_units = 0.001
+                self.marker_units = 0.001
             if len(self.model_marker_names) > len(exp_marker_names):
                 supplementary_marker_names = [name for name in self.model_marker_names if name not in exp_marker_names]
                 raise ValueError(f"The markers {supplementary_marker_names} are not in the c3d file, but are in the model.")
@@ -87,7 +91,7 @@ class ExperimentalData:
             markers_sorted[:, :, :] = np.nan
             for i_marker, name in enumerate(exp_marker_names):
                 marker_idx = self.model_marker_names.index(name)
-                markers_sorted[:, marker_idx, :] = markers[:3, i_marker, :] * marker_units
+                markers_sorted[:, marker_idx, :] = markers[:3, i_marker, :] * self.marker_units
             self.markers_sorted = markers_sorted
 
         def add_virtual_markers():
@@ -105,88 +109,78 @@ class ExperimentalData:
             self.markers_sorted_with_virtual = markers_sorted_with_virtual
 
         def sort_analogs():
+            """
+            TODO: -> treatment of the EMG signal to remove stimulation artifacts here
+            """
+
             # Get an array of the experimental muscle activity
             analogs = self.c3d["data"]["analogs"]
             self.nb_analog_frames = analogs.shape[2]
             self.analogs_sampling_frequency = self.c3d["parameters"]["ANALOG"]["RATE"]["value"][0]  # Hz
             self.analogs_dt = 1 / self.c3d["header"]["analogs"]["frame_rate"]
             analog_names = self.c3d["parameters"]["ANALOG"]["LABELS"]["value"]
-            analog_units = np.ones((len(analog_names, )))
-            if self.c3d["parameters"]["ANALOG"]["UNITS"]["value"][0] == "mm":
-                analog_units = 0.001
+
             # print(analog_names)
             # emg_sorted = np.zeros((len(model_muscle_names), self.nb_analog_frames))
             # for i_muscle, name in enumerate(model_muscle_names):
             #     muscle_idx = analog_names.index(name)
             #     emg_sorted[i_muscle, :] = analogs[muscle_idx, :]
             # self.emg_sorted = emg_sorted
-            # # TODO: Charbie -> treatment of the EMG signal to remove stimulation artifacts
-            return analog_names, analogs
+            return
 
-        def extract_force_platform_data(analog_names, analogs):
-            # TODO: Find how to get "platform" field !!!!! (@Ophlariviere)
-            # TODO: Charbie/Flo -> make sure this is generalizable to Vicon
-            # Get the experimental ground reaction forces
-            # 0:3 -> forces, 3:6 -> moments
-            force_platform_1_channels = self.c3d["parameters"]["FORCE_PLATFORM"]["CHANNEL"]["value"][:, 0]
-            force_platform_2_channels = self.c3d["parameters"]["FORCE_PLATFORM"]["CHANNEL"]["value"][:, 1]
-            force_platform_1_zeros = self.c3d["parameters"]["FORCE_PLATFORM"]['ZERO']['value'][0]
-            force_platform_2_zeros = self.c3d["parameters"]["FORCE_PLATFORM"]['ZERO']['value'][1]
-            force_platform_1_calibration_matrix = self.c3d["parameters"]["FORCE_PLATFORM"]['CAL_MATRIX']['value'][:, :, 0]
-            force_platform_2_calibration_matrix = self.c3d["parameters"]["FORCE_PLATFORM"]['CAL_MATRIX']['value'][:, :, 1]
-            f_ext = np.zeros((2, 6, self.nb_analog_frames))
-            for i in range(6):
-                platform_1_idx = analog_names.index(f"Channel_{force_platform_1_channels[i]:02d}")
-                platform_2_idx = analog_names.index(f"Channel_{force_platform_2_channels[i]:02d}")
-                f_ext[0, i, :] = analogs[0, platform_1_idx, :]
-                f_ext[1, i, :] = analogs[0, platform_2_idx, :]
-            f_ext_sorted = np.zeros((2, 9, self.nb_analog_frames))
-            f_ext_sorted[0, 3:, :] = np.vstack((f_ext[0, 3:6, :], f_ext[0, :3, :]))
-            f_ext_sorted[1, 3:, :] = np.vstack((f_ext[1, 3:6, :], f_ext[1, :3, :]))
-
-            # Apply calibration matrix and zero
-            # TODO: Charbie -> to be removed ! -------------------------------- # not the right order anymore !
-            # f_ext_sorted[0, :, :] = force_platform_1_calibration_matrix @ (f_ext_sorted[0, :, :] - force_platform_1_zeros)
-            # f_ext_sorted[1, :, :] = force_platform_2_calibration_matrix @ (f_ext_sorted[1, :, :] - force_platform_2_zeros)
-            # Modify moment units from mm to m
-            # f_ext_sorted[:, 3:6 :] /= 1000
-            # Get the force plate origin
-            force_platform_1_origin = np.mean(self.c3d["parameters"]["FORCE_PLATFORM"]['CORNERS']['value'][:, :, 0], axis=1)
-            force_platform_2_origin = np.mean(self.c3d["parameters"]["FORCE_PLATFORM"]['CORNERS']['value'][:, :, 1], axis=1)
-            """"
-            center_of_pressure_1 = self.c3d["data"]["platform"]['center_of_pressure'] / 1000  # .....
-            center_of_pressure_2 = self.c3d["data"]["platform"]['center_of_pressure'] / 1000  # .....
-            distance_cop_to_origin_1 = force_platform_1_origin - center_of_pressure_1
-            distance_cop_to_origin_2 = force_platform_2_origin - center_of_pressure_2
-            transportation_moment_1 = np.cross(distance_cop_to_origin_1, f_ext_sorted[0, 3:6, :])
-            transportation_moment_2 = np.cross(distance_cop_to_origin_2, f_ext_sorted[1, 3:6, :])
-            f_ext_sorted[0, 6:9, :] += transportation_moment_1
-            f_ext_sorted[1, 6:9, :] += transportation_moment_2
-            f_ext_sorted[0, :3, :] = center_of_pressure_1
-            f_ext_sorted[1, :3, :] = center_of_pressure_2
+        def extract_force_platform_data():
             """
+            Extracts the force platform data from the c3d file and filters it.
+            The F_ext output is of the form [cop, moments, forces].
+            """
+
+            platforms = self.c3d["data"]["platform"]
+            nb_platforms = len(platforms)
+            units = self.marker_units  # We assume that the all position units are the same as the markers'
+
+            # Initialize arrays for storing external forces and moments
+            platform_origin = np.zeros((nb_platforms, 3, 1))
+            moment_adjusted = np.zeros((nb_platforms, 3, self.nb_analog_frames))
+            force_filtered = np.zeros((nb_platforms, 3, self.nb_analog_frames))
+            moment_filtered = np.zeros((nb_platforms, 3, self.nb_analog_frames))
+            cop_filtered = np.zeros((nb_platforms, 3, self.nb_analog_frames))
+            moment_adjusted_filtered = np.zeros((nb_platforms, 3, self.nb_analog_frames))
+            f_ext_sorted = np.zeros((2, 9, self.nb_analog_frames))
+            f_ext_sorted_filtered = np.zeros((2, 9, self.nb_analog_frames))
+
+            # Process force platform data
+            for i_platform in range(nb_platforms):
+
+                # Get the data
+                force = platforms[i_platform]['force']
+                moment = platforms[i_platform]['moment'] * units
+                cop = platforms[i_platform]['center_of_pressure'] * units
+
+                # Filter center of pressure data
+                cop_filtered[i_platform] = Operator.apply_filtfilt(cop, order=4, sampling_rate=self.analogs_sampling_frequency, cutoff_freq=10)
+
+                # Modify moments to express them at the center of the cop
+                platform_origin[i_platform, :, 0] = np.mean(platforms[i_platform]['corners'] * units, axis=1)
+                for i_frame in range(self.nb_analog_frames):
+                    r = platform_origin[i_platform, :, 0] - cop_filtered[i_platform, :, i_frame]
+                    moment_offset = np.cross(r, force[i_frame])
+                    moment_adjusted[i_platform, :, i_frame] = moment[i_frame] + moment_offset
+
+                # Filter forces and moments
+                force_filtered[i_platform, :, :] = Operator.apply_filtfilt(force, order=4, sampling_rate=self.analogs_sampling_frequency, cutoff_freq=10)
+                moment_filtered[i_platform, :, :] = Operator.apply_filtfilt(moment, order=4, sampling_rate=self.analogs_sampling_frequency, cutoff_freq=10)
+                moment_adjusted_filtered[i_platform, :, :] = Operator.apply_filtfilt(moment_adjusted[i_platform, :, :], order=4, sampling_rate=self.analogs_sampling_frequency, cutoff_freq=10)
+
+                # Store output in a biorbd compatible format
+                f_ext_sorted[i_platform, :3, :] = cop[i_platform, :, :]
+                f_ext_sorted_filtered[i_platform, :3, :] = cop_filtered[i_platform, :, :]
+                f_ext_sorted[i_platform, 3:6, :] = moment_adjusted[i_platform, :, :]
+                f_ext_sorted_filtered[i_platform, 3:6, :] = moment_adjusted_filtered[i_platform, :, :]
+                f_ext_sorted[i_platform, 6:9, :] = force[i_platform, :, :]
+                f_ext_sorted_filtered[i_platform, 6:9, :] = force_filtered[i_platform, :, :]
+
             self.f_ext_sorted = f_ext_sorted
-
-            # f_extfilt[contact, :, :] = self.forcedatafilter(f_ext, 4, 2000, 20)
-            # moment_extfilt[contact, :, :] = self.forcedatafilter(moment_ext, 4, 2000, 20)
-            # cop_ext = self.c3d['data']['platform'][contact]['center_of_pressure'] / 1000
-            # cop_extfilt[contact, :, :] = self.forcedatafilter(cop_ext, 4, 2000, 10)
-
-            # import matplotlib.pyplot as plt
-            # plt.figure()
-            # plt.plot(np.linalg.norm(f_ext_sorted[0, 0:3, :], axis=0))
-            # plt.plot(np.linalg.norm(f_ext_sorted[0, 3:6, :], axis=0))
-            # plt.plot(np.linalg.norm(f_ext_sorted[1, 0:3, :], axis=0))
-            # plt.plot(np.linalg.norm(f_ext_sorted[1, 3:6, :], axis=0))
-            # plt.plot(np.linalg.norm(f_ext_sorted[0, 0:3, :], axis=0) + np.linalg.norm(f_ext_sorted[1, 0:3, :], axis=0))
-            # plt.plot(np.linalg.norm(f_ext_sorted[0, 3:6, :], axis=0) + np.linalg.norm(f_ext_sorted[1, 3:6, :], axis=0))
-            # plt.savefig('test.png')
-            # plt.show()
-
-            # from scipy import signal
-            # b, a = signal.butter(2, 1/50, btype='low')
-            # y = signal.filtfilt(b, a, f_ext_sorted[0, 8, :], padlen=150)
-            # # 4th 6-10
+            self.f_ext_sorted_filtered = f_ext_sorted_filtered
 
 
         def compute_time_vectors():
@@ -197,8 +191,8 @@ class ExperimentalData:
         load_model()
         sort_markers()
         add_virtual_markers()
-        analog_names, analogs = sort_analogs()
-        extract_force_platform_data(analog_names, analogs)
+        sort_analogs()
+        extract_force_platform_data()
         compute_time_vectors()
 
 
@@ -249,6 +243,7 @@ class ExperimentalData:
             "analogs_dt": self.analogs_dt,
             "nb_analog_frames": self.nb_analog_frames,
             "f_ext_sorted": self.f_ext_sorted,
+            "f_ext_sorted_filtered": self.f_ext_sorted_filtered,
             "markers_time_vector": self.markers_time_vector,
             "analogs_time_vector": self.analogs_time_vector,
         }
--- gait_analyzer/operator.py
+++ gait_analyzer/operator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import butter, filtfilt
 
 
 class Operator:
@@ -41,4 +42,38 @@ class Operator:
             else:
                 x_averaged[i] = np.mean(x[i - window_size // 2 : i + window_size // 2 + 1])
         return x_averaged
+
+
+    @staticmethod
+    def apply_filtfilt(data: np.ndarray, order: int, sampling_rate: int, cutoff_freq: int):
+        """
+        TODO: @ophlariviere -> This was taken from biomechanics tools, could you provide a ref for it ?
+        .
+        Apply a low-pass Butterworth filter to the data using scipy.filtfilt
+        .
+        Parameters
+        ----------
+        data: np.ndarray
+            The data to be filtered (for now, this can only be a nb_data x nb_frames array)
+        order: int
+            The order of the Butterworth filter
+        sampling_rate: int
+            The sampling rate of the data in Hz
+        cutoff_freq: int
+            The cutoff frequency of the filter in Hz
+        .
+        Returns
+        -------
+        filtered_data: np.ndarray
+            The filtered data
+        """
+        nyquist = 0.5 * sampling_rate
+        normal_cutoff = cutoff_freq / nyquist
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        filtered_data = np.zeros_like(data)
+        filtered_data[:, :] = np.nan
+        for i_data in range(data.shape[0]):
+            non_nan_idx = ~np.isnan(data[i_data, :])
+            filtered_data[i_data, non_nan_idx] = filtfilt(b, a,  data[i_data, non_nan_idx], axis=0)
+        return filtered_data
 
